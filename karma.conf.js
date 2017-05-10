da4f24d2b881cb1eacf9e0bdc26435d3ded38ffd
Beasta <barryblaha@gmail.com>
2017-05-10 11:29:29 -0700

    Update reference to @angular/cli from angular-cli in karma.conf

--- karma.conf.js
+++ karma.conf.js
@@ -4,7 +4,7 @@
 module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine', 'angular-cli'],
+    frameworks: ['jasmine', '@angular/cli'],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
