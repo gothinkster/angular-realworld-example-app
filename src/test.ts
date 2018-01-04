044c174f24f6971f00b62ed2d499600f10f6b4dd
Juri <juri.strumpflohner@r3-gis.com>
2018-01-04 22:40:58 +0100

    upgrade to latest Angular version + CLI changes

--- src/test.ts
+++ src/test.ts
@@ -1,3 +1,5 @@
+// This file is required by karma.conf.js and loads recursively all the .spec and framework files
+
 import 'zone.js/dist/long-stack-trace-zone';
 import 'zone.js/dist/proxy.js';
 import 'zone.js/dist/sync-test';
@@ -11,18 +13,17 @@ import {
 } from '@angular/platform-browser-dynamic/testing';
 
 // Unfortunately there's no typing for the `__karma__` variable. Just declare it as any.
-declare var __karma__: any;
-declare var require: any;
+declare const __karma__: any;
+declare const require: any;
 
 // Prevent Karma from running prematurely.
-__karma__.loaded = function () { };
+__karma__.loaded = function () {};
 
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting()
 );
-
 // Then we find all the tests.
 const context = require.context('./', true, /\.spec\.ts$/);
 // And load the modules.
