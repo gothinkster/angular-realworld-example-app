05e51bfb4c829b9921bc16be562d0b9cf034d4ce
Pedro <pgbonino@gmail.com>
2018-03-09 14:56:48 +0100

    Remove local backend specification

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,5 @@
 
 export const environment = {
   production: false,
-  //api_url: 'https://conduit.productionready.io/api'
-  api_url: 'http://localhost:3000/api'
+  api_url: 'https://conduit.productionready.io/api'
 };
