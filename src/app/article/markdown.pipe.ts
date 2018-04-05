07884c605552463800d2610bfab9adf6b64b7bc7
jafaircl <jafaircl@me.com>
2018-04-05 17:25:43 -0400

    transform returns a promise, not a string

--- src/app/article/markdown.pipe.ts
+++ src/app/article/markdown.pipe.ts
@@ -2,7 +2,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({name: 'markdown'})
 export class MarkdownPipe implements PipeTransform {
-  async transform(content: string): string {
+  async transform(content: string): Promise<string> {
     const marked = await import('marked');
     return marked(content, { sanitize: true });
   }
