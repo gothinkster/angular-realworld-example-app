8ae9510dcdef780b4cc20bed33292283ff81bb3d
Freax <huangyanxiong2013@gmail.com>
2018-10-17 19:57:53 +0800

    Remove useless module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { ModuleWithProviders, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
