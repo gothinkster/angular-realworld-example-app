098a2094d685f3f702c1335d3d9af85b992e484c
Khaled Osman <khaled.osmaan@gmail.com>
2019-04-09 10:06:26 +0200

    disable ivy since lazyloading is currently not working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,19 +4,19 @@ import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 const routes: Routes = [
   {
     path: 'settings',
-    loadChildren: () => import('./settings/settings.module').then(m => m.SettingsModule)
+    loadChildren: './settings/settings.module#SettingsModule'
   },
   {
     path: 'profile',
-    loadChildren: () => import('./profile/profile.module').then(m => m.ProfileModule)
+    loadChildren: './profile/profile.module#ProfileModule'
   },
   {
     path: 'editor',
-    loadChildren: () => import('./editor/editor.module').then(m => m.EditorModule)
+    loadChildren: './editor/editor.module#EditorModule'
   },
   {
     path: 'article',
-    loadChildren: () => import('./article/article.module').then(m => m.ArticleModule)
+    loadChildren: './article/article.module#ArticleModule'
   }
 ];
 
