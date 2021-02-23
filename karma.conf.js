ec7168ca90c54f30ca8a69f2e01a9494547d507b
RuiVit <8190479@estg.ipp.pt>
2021-02-23 13:59:11 +0000

    del

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,5 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
-// will it be
 module.exports = function(config) {
   config.set({
     basePath: '',
