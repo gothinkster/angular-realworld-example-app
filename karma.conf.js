5a4a8ea99b834a2d9ac1b2d00e1d9bd51960af12
Rui Vitorino <8190479@estg.ipp.pt>
2021-02-23 15:04:06 +0000

    ok

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,6 @@
 // Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.htmljjhj
+// https://karma-runner.github.io/1.0/config/configuration-file.html
+
 module.exports = function(config) {
   config.set({
     basePath: '',
