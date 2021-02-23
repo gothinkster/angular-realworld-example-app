246bcbc07e2723af3c74674596e1f64b8d7b2583
Rui Vitorino <8190479@estg.ipp.pt>
2021-02-23 09:29:05 +0000

    testcommit
    no desc

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,6 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
-
+// added comment
 module.exports = function(config) {
   config.set({
     basePath: '',
