7ee55e25e4bb147f98a07f1b52c371bd2e2fd165
Rui Vitorino <8190479@estg.ipp.pt>
2021-02-23 14:11:38 +0000

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,5 @@
 // Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
+// https://karma-runner.github.io/1.0/config/configuration-file.htmljjhj
 module.exports = function(config) {
   config.set({
     basePath: '',
