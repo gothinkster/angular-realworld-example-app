4207e707a9e72879c0e2db704b41b0aec6d4bd97
Rui Vitorino <8190479@estg.ipp.pt>
2021-02-24 11:38:49 +0000

    TriggerTest2

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,6 @@
 // Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html 
+// https://karma-runner.github.io/1.0/config/configuration-file.html
+// Comment 
 
 module.exports = function(config) {
   config.set({
