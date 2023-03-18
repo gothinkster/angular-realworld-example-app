db8c6b02fb4df07203503a1cbf2b3239e32445ed
geromegrignon <gerome.grignon.lp2@gmail.com>
2023-03-18 02:34:32 +0100

    chore: update to angular v13

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -18,22 +18,12 @@
 * BROWSER POLYFILLS
 */
 
-
-/** IE10 and IE11 requires the following for NgClass support on SVG elements */
-// import 'classlist.js';  // Run `npm install --save classlist.js`.
-
 /** IE10 and IE11 requires the following for the Reflect API. */
 // import 'core-js/es6/reflect';
 
 /** Evergreen browsers require these. **/
 // Used for reflect-metadata in JIT. If you use AOT (and only Angular decorators), you can remove.
 import 'core-js/es7/reflect';
-
-/**
- * Required to support Web Animations `@angular/platform-browser/animations`.
- * Needed for: All but Chrome, Firefox and Opera. http://caniuse.com/#feat=web-animation
- **/
-// import 'web-animations-js';  // Run `npm install --save web-animations-js`.
 
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
