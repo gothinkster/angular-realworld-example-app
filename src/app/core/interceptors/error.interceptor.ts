ddc2f9888438fac0f6680b86f36a648f4d8ab92a
Praise Mayone <terrymayone@gmail.com>
2023-08-01 13:07:12 +0100

    [update] Updated the http error interceptor

--- src/app/core/interceptors/error.interceptor.ts
+++ src/app/core/interceptors/error.interceptor.ts
@@ -6,14 +6,23 @@ import {
   HttpRequest,
 } from "@angular/common/http";
 import { Observable, throwError } from "rxjs";
+import { Router } from "@angular/router"
 import { catchError } from "rxjs/operators";
+import { UserService } from "../services/user.service";
 
 @Injectable({ providedIn: "root" })
 export class ErrorInterceptor implements HttpInterceptor {
+  constructor(private authSvc: UserService) { }
   intercept(
     req: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
-    return next.handle(req).pipe(catchError((err) => throwError(err.error)));
+    return next.handle(req).pipe(catchError((err) => {
+      if ([401, 403].includes(JSON.parse(err.status))) {
+        this.authSvc.logout()
+      }
+      const error = err.error.message || err.statusText
+      throwError(() => error)
+    }));
   }
 }
