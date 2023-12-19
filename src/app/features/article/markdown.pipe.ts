c5c6568d81d2b76cf15c48abf1b5ee6431d3b416
Gerome Grignon <gerome.grignon.lp2@gmail.com>
2023-12-19 07:31:46 +0100

    fix: sanitize content

--- src/app/features/article/markdown.pipe.ts
+++ src/app/features/article/markdown.pipe.ts
@@ -1,12 +1,17 @@
-import { Pipe, PipeTransform } from "@angular/core";
+import { inject, Pipe, PipeTransform, SecurityContext } from "@angular/core";
+import { DomSanitizer } from "@angular/platform-browser";
 
 @Pipe({
   name: "markdown",
   standalone: true,
 })
 export class MarkdownPipe implements PipeTransform {
+  domSanitizer = inject(DomSanitizer);
   async transform(content: string): Promise<string> {
     const { marked } = await import("marked");
-    return marked(content, { sanitize: true });
+    return (
+      this.domSanitizer.sanitize(SecurityContext.HTML, marked.parse(content)) ||
+      ""
+    );
   }
 }
